a21cd6a0a76aaa976b945382d05e884d3fcb85ff
mitant127 <55878970+mitant127@users.noreply.github.com>
2019-11-21 23:17:52 +0300

    Change task

--- Lesson-4/MaxNeighbour.py
+++ Lesson-4/MaxNeighbour.py
@@ -5,7 +5,6 @@
 
 
 my_list = [4, 32, 98, 12, 11, 56, 73, 22]
-new_list = [el for el in my_list if my_list.index(el) < (len(my_list)) if my_list[my_list.index(el)] > my_list[my_list.index(el) - 1]]
-# new_list = [el for el in my_list ]
-
+new_list = [el for el in my_list if my_list.index(el) < (len(my_list))
+            if my_list[my_list.index(el)] > my_list[my_list.index(el) - 1]]
 print(new_list)
